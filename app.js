79525fdf743c63ce1905f69875825158f4db8efe
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-17 17:46:53 +0800

    feat: add detail / login / register page

--- app.js
+++ app.js
@@ -7,15 +7,59 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
+const db = require('./models')
+const User = db.User
+const Todo = db.Todo
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// 首頁
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
-app.listen(PORT, () => [
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmpassworf } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
+
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id) // find by primary-key
+    .then(todo => res.render('detail', { todo: todo.toJSON() })) // to transfer plain object
+    .catch(error => console.log(error))
+})
+
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
-])
+})
