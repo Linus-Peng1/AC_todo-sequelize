6088a45b9bcf2deadc0c7c4582d158d23087d09d
Linus-Peng1 <snoopypeng1217@gmail.com>
2021-08-19 13:58:52 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,13 +3,20 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 // 首頁
 router.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
+  User.findByPk(req.user.id) // user 已在 passport 被存進 req
+    .then(user => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
+    })
     .then((todos) => { return res.render('index', { todos }) })
     .catch((error) => { return res.status(422).json(error) })
 })
--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,8 +6,11 @@ const Todo = db.Todo
 
 // 詳細資料
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id) // find by primary-key
+  return Todo.findOne({
+    where: { id, UserId } // 尋找符合 id = id 及 UserId = UserId 的 todo
+  })
     .then(todo => res.render('detail', { todo: todo.toJSON() })) // to transfer plain object
     .catch(error => console.log(error))
 })
